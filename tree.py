dd8b364c89873691e10366168aae51e38d0ebe6f
S K Fung <sk_fung@SKF-MacBook-Air.local>
2020-04-25 23:41:02 +0800

    add write to txt, csv, tsv options

--- tree.py
+++ tree.py
@@ -7,6 +7,7 @@ import argparse
 
 # get_dirlist scans the current_dir using path_obj.iterdir() (available from pathlib) and
 # construct the directory tree recursively using ├ , ─ , └ characters
+# and a list of the tree will be returned
 #
 # current_dir : the current directory to be scanned
 # depth : number of sub-levels to be included in directory tree. When depth=1, will discontinue sub-directory scan
@@ -15,7 +16,7 @@ import argparse
 # indent_space : the repetition before and after ├ (that is space and ─) for construction of next_indent. Minimum
 #     is 1, default is 3. It is also equal to size of indent for each subdirectory
 # folder_only : =True if only directory is shown in result
-# silent_mode : default is False to print directory tree to screen. True to suppress print to screen
+# include_period : incldue files with . as prefix (usually hidden). Default is False
 # prefix_dir_char : The prefix character add before the folder name. Default is the folder emoji
 # prefix_file_char : The prefix character add before non-folder name. Default is memo emoji.
 
@@ -24,9 +25,9 @@ def get_dirlist(current_dir,
                 depth,
                 indent="",
                 indent_space=2,
+                indent_prefix="",
                 folder_only=False,
                 include_period=False,
-                silent_mode=False,
                 prefix_dir_char="\N{OPEN FILE FOLDER}",
                 prefix_file_char="\N{MEMO} "):
 
@@ -49,7 +50,7 @@ def get_dirlist(current_dir,
         indent_space = 1
 
     if indent == "":
-        indent = " "*indent_space + "├" + "─"*indent_space
+        indent = indent_prefix + " "*indent_space + "├" + "─"*indent_space
 
     subitem_count = 0
     for subitem in current_dir.iterdir():
@@ -63,11 +64,12 @@ def get_dirlist(current_dir,
         # scan, change the tree character from ├" to "└"
         is1 = indent_space + 1
         if subitem_count == (subitem_total):    # last item in the subdir
-            next_indent = indent[:-(is1)] + " "*(is1) + indent[-(is1):]
+            next_indent = indent[:-(is1)] + indent_prefix + \
+                " "*(is1) + indent[-(is1):]
             indent = indent[:-(is1)] + "└" + "─"*(is1-1)
         else:    # not the last item in the subdir
             next_indent = indent[:-(is1)] + \
-                "│" + " "*indent_space + indent[-(is1):]
+                "│" + indent_prefix + " "*indent_space + indent[-(is1):]
         if depth >= 1:  # not yet end of recursive sub-levels
             # determine leading character before directory / file name
             if subitem.is_file():
@@ -76,8 +78,7 @@ def get_dirlist(current_dir,
                 leading_char = prefix_dir_char
             else:
                 leading_char = ""
-            if not silent_mode:
-                print(f"{indent}{leading_char}{subitem.name}")
+            # print(f"{indent}{leading_char}{subitem.name}")
             result_list.append(f"{indent}{leading_char}{subitem.name}\n")
 
             # Recursively scan sub-directory
@@ -86,9 +87,9 @@ def get_dirlist(current_dir,
                                                depth-1,
                                                next_indent,
                                                indent_space,
+                                               indent_prefix,
                                                folder_only,
                                                include_period,
-                                               silent_mode,
                                                prefix_dir_char,
                                                prefix_file_char))
     return result_list
@@ -100,6 +101,7 @@ my_parser = argparse.ArgumentParser(
     description="List folder content recursively in tree structure",
     # epilog="For any question, pls email to me",
     allow_abbrev=False)
+my_group = my_parser.add_mutually_exclusive_group(required=False)
 
 my_parser.add_argument("path", nargs='?', metavar="PATH",
                        action="store",
@@ -130,17 +132,13 @@ my_parser.add_argument("-s", "--silent",
                        action="store_true",
                        required=False,
                        help="Don't show result on screen")
-my_parser.add_argument("-w", "--writetofile",
-                       action="store_true",
-                       required=False,
-                       help="Write the tree to file")
 my_parser.add_argument("-o", "--outputfile", metavar="FILENAME",
                        action="store",
                        type=str,
-                       default="FOLDER_LIST.txt",
+                       default="FOLDER_LIST",
                        required=False,
                        help="Specify output file name if \
-                       other than FOLDER_LIST.txt")
+                       other than 'FOLDER_LIST'")
 my_parser.add_argument("-e", "--emoji",
                        action="store_true",
                        required=False,
@@ -149,7 +147,19 @@ my_parser.add_argument("-v", "--version",
                        action="version",
                        version="0.1.0")
 
+my_group.add_argument("-t", "--textfile",
+                      action="store_true",
+                      help="Write to text file")
+my_group.add_argument("-c", "--csv",
+                      action="store_true",
+                      help="Write to csv file")
+my_group.add_argument("-b", "--tab",
+                      action="store_true",
+                      help="Write to tsv file")
+
+
 args = my_parser.parse_args()
+
 input_path = Path(args.path)
 full_path = input_path.resolve(strict=False)
 
@@ -165,22 +175,45 @@ if not input_path.exists():
 elif not input_path.is_dir():
     print(f"{args.path} is not directory")
 else:
-    output_file = args.outputfile
-    if output_file.lower().endswith(".txt"):
-        original_file = output_file[:-4]
+    writetofile = True
+    delimiter = ""
+    if args.textfile:
+        ext = ".txt"
+    elif args.csv:
+        ext = ".csv"
+        delimiter = ","
+    elif args.tab:
+        ext = ".tsv"
+        delimiter = "\t"
     else:
-        original_file = output_file
-        output_file = original_file + ".txt"
-
-    n = 0
-    while Path(output_file).exists():
-        output_file = f"{original_file}_{n:03}.txt"
-        n += 1
+        writetofile = False
 
     if not args.silent:
         print(f"\n{str(full_path)}")
+        for line in get_dirlist(input_path, args.depth, indent="",
+                                indent_space=args.indent,
+                                indent_prefix="",
+                                folder_only=args.folderonly,
+                                include_period=args.period,
+                                prefix_dir_char=folder_char,
+                                prefix_file_char=file_char):
+            print(line, end="")
 
-    if args.writetofile:
+    if writetofile:
+        output_file = args.outputfile
+        if output_file.lower().endswith(ext):
+            original_file = output_file[:-4]
+        else:
+            original_file = output_file
+            output_file = original_file + ext
+
+        n = 0
+        while Path(output_file).exists():
+            output_file = f"{original_file}_{n:03}{ext}"
+            n += 1
+
+        print(f"\nWriting folder tree to file {str(output_file)}")
+
         with open(output_file, mode="w", encoding="utf-8-sig") as f:
             f.write(
                 f"Directory list of {args.path} upto {args.depth} sub-level(s)\n")
@@ -188,18 +221,10 @@ else:
             f.write(f"{str(full_path)}\n")
             f.writelines(get_dirlist(input_path, args.depth, indent="",
                                      indent_space=args.indent,
+                                     indent_prefix=delimiter,
                                      folder_only=args.folderonly,
                                      include_period=args.period,
-                                     silent_mode=args.silent,
                                      prefix_dir_char=folder_char,
                                      prefix_file_char=file_char))
         print(
-            f"\nDirectory list is written to text file: {str(full_path)}\n")
-    else:
-        get_dirlist(input_path, args.depth, indent="",
-                    indent_space=args.indent,
-                    folder_only=args.folderonly,
-                    include_period=args.period,
-                    silent_mode=args.silent,
-                    prefix_dir_char=folder_char,
-                    prefix_file_char=file_char)
+            f"\nFolder tree has been written to file: {output_file}\n")
